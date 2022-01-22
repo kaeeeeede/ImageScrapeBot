62395ad1c57a029886c65c30d7d818ff5d3c7fe0
kaeeeeede <kaedewrites1@gmail.com>
2022-01-23 05:24:12 +0800

    working iteration of image_scraper

--- image_scraper.py
+++ image_scraper.py
@@ -9,17 +9,22 @@ def getData(url):
     r = requests.get(url)
     soup = BeautifulSoup(r.text, 'html.parser')
     images = soup.findAll('img')
-    create_folder(images)
+    
+    return images
 
 def create_folder(images):
     try:
         os.mkdir(folder_name)
     except:
         pass
-    download_images(images, folder_name)
+    
+    return folder_name
 
-def download_images(images, folder_name):
-    if len(images) != 0:
+def download_images(images, folder_name, file_size_limit, file_count_limit):
+    total_file_size = 0
+    total_file_number = 0
+
+    while total_file_size < file_size_limit and total_file_number < file_count_limit:
         for i, image in enumerate(images):
             if image.get("data-srcset") != None:
                 image_link = image["data-srcset"]
@@ -35,9 +40,14 @@ def download_images(images, folder_name):
             r = requests.get(image_link).content
             with open(f"{folder_name}/images{i+1}.jpg", "wb+") as f:
                 f.write(r)
-                img.append(f"{folder_name}/images{i+1}.jpg")
+                total_file_size += os.path.getsize(f"{folder_name}/images{i+1}.jpg")
+                total_file_number += 1
+                if total_file_size < file_size_limit and total_file_number < file_count_limit:
+                    img.append(f"{folder_name}/images{i+1}.jpg")
+                else:
+                    break
 
-    return(img)
+    return img
 
 if __name__ == "__main__":
     address = input("Address:- ")
